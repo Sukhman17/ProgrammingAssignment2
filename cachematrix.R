4ad178c88fedeabd1ae64b0883d2cfb155e98e2e
Sukhman17 <sukhmank71@gmail.com>
2015-07-20 21:07:03 +0530

    Revert "Updated Code"
    This reverts commit a2cd727fd6dd3a2c2042689f1bdd69b825b96faf.

--- cachematrix.R
+++ cachematrix.R
@@ -3,51 +3,13 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {## @x: a square invertible matrix
-        ## return: a list containing functions to
-        ##              1. set the matrix
-        ##              2. get the matrix
-        ##              3. set the inverse
-        ##              4. get the inverse
-        ##         this list is used as the input to cacheSolve()
-        
-        inv = NULL
-        set = function(y) {
-                # use `<<-` to assign a value to an object in an environment 
-                # different from the current environment. 
-                x <<- y
-                inv <<- NULL
-        }
-        get = function() x
-        setinv = function(inverse) inv <<- inverse 
-        getinv = function() inv
-        list(set=set, get=get, setinv=setinv, getinv=getinv)
-}
+makeCacheMatrix <- function(x = matrix()) {
 
+}
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-## @x: a square invertible matrix
-        ## return: a list containing functions to
-        ##              1. set the matrix
-        ##              2. get the matrix
-        ##              3. set the inverse
-        ##              4. get the inverse
-        ##         this list is used as the input to cacheSolve()
-        
-        inv = NULL
-        set = function(y) {
-                # use `<<-` to assign a value to an object in an environment 
-                # different from the current environment. 
-                x <<- y
-                inv <<- NULL
-        }
-        get = function() x
-        setinv = function(inverse) inv <<- inverse 
-        getinv = function() inv
-        
         ## Return a matrix that is the inverse of 'x'
-		list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
